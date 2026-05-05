57a96ebc674c3465c52d2b1de382eabc0b73fa01
Mahak Gupta <mahakg2588@gmail.com>
2026-05-05 15:20:23 +0530

    record

--- backend/src/main/java/com/cdrn/repository/OtpRecordRepository.java
+++ backend/src/main/java/com/cdrn/repository/OtpRecordRepository.java
@@ -4,7 +4,7 @@ import com.cdrn.model.OtpRecord;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.Optional;
-
+//otp record repository
 public interface OtpRecordRepository extends MongoRepository<OtpRecord, String> {
     Optional<OtpRecord> findTopByPhoneOrderByCreatedAtDesc(String phone);
 }
