4b022104dc15de5287c8397af2057c0517d3fe0c
NAVEEN SHEKHAR SRIVASTAVA <naveen.shekhar.sri@gmail.com>
2026-05-05 15:14:46 +0530

    Delete AlertRepository.java

--- AlertRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cdrn.repository;
-
-import com.cdrn.domain.Alert;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AlertRepository extends JpaRepository<Alert, Long> {
-}
