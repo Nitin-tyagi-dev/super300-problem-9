52a295c396e686b176b2d6ff74240adb69dcd236
NAVEEN SHEKHAR SRIVASTAVA <naveen.shekhar.sri@gmail.com>
2026-05-05 15:15:37 +0530

    Delete RecoveryAssessmentRepository.java

--- RecoveryAssessmentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cdrn.repository;
-
-import com.cdrn.domain.RecoveryAssessment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RecoveryAssessmentRepository extends JpaRepository<RecoveryAssessment, Long> {
-}
