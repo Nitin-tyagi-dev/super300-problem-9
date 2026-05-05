edc9954a012678a4d2829673357ced6dcaf4a28c
lokesh singh <lokeshthakur8954@gmail.com>
2026-05-05 15:19:52 +0530

    app and css created

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { Route, Routes } from "react-router"
+import Registration from "./pages/Registration"
+import Login from "./pages/Login"
+import Home from "./pages/Home"
+function App() {
+  return (
+    <>
+    <Routes>
+      <Route path="/registration" element={<Registration/>}/>
+      <Route path="/login" element={<Login/>}/>
+      <Route path="/" element={<Home/>}/>
+    </Routes>
+    </>
+  )
+}
+
+export default App
