8f4bb2200f17f97ca13fe599283c70030722c068
NAVEEN SHEKHAR SRIVASTAVA <naveen.shekhar.sri@gmail.com>
2026-05-05 15:15:00 +0530

    Delete IncidentRepository.java

--- IncidentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.cdrn.repository;
-
-import com.cdrn.domain.Incident;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IncidentRepository extends JpaRepository<Incident, Long> {
-    long countByStatus(String status);
-}
